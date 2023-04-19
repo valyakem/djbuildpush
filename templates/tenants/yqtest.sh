68fb4131551726c181bdccd265bf0ade49e52ff5
valyakem <valyakem@yahoo.co.uk>
2023-04-19 09:51:46 -0400

    updated yq

--- templates/tenants/yqtest.sh
+++ templates/tenants/yqtest.sh
@@ -1,35 +1,14 @@
-inputfile="templates/tenants/customers.yaml"
-getusers="$(yq '.deployment.[].name' $inputfile)"
-#echo $getcustomers
-#extract all generic key values and store as variables
-gizmoImageVersion="$(yq '.gizmoImageVersion' $inputfile)"
-robotManagerImageVersion="$(yq '.robotManagerImageVersion' $inputfile)"
-iqImageVersion="$(yq '.iqImageVersion' $inputfile)"
-keycloakImageVersion="$(yq '.keycloakImageVersion' $inputfile)"
-npvImageVersion="$(yq '.npvImageVersion' $inputfile)"
-elasticSearchVersion="$(yq '.elasticSearchVersion' $inputfile)"
-
-#get an array list of all customers by name
-getcustomers="$(yq '.deployment.[].name' $inputfile)"
-
-#loop through the list of array one after the order
-for customer in $getcustomers
-  do
-         var=$((var+1))  #add 1 to var each time the loop is done
-               
-               #get corresponding values based on the array index and assign them to variables
-               getcustname="$(yq '.[].customer'$var'.name' $inputfile)"
-               getcustsize="$(yq '.[].customer'$var'.size' $inputfile)"
-               getcusttenant="$(yq '.[].customer'$var'.tenantname' $inputfile)"
-               getcustdeploygroup="$(yq '.[].customer'$var'.deploymentgroup' $inputfile)"
-
-         #output the values on the terminal
-              echo $getcustname "|" $gizmoImageVersion "|" $robotManagerImageVersion "|" \
-              $iqImageVersion  "|" $keycloakImageVersion "|" $npvImageVersion "|" \
-              $elasticSearchVersion "|" $getcustsize "|" $getcusttenant "|" $getcustdeploygroup
-              echo "##vso[task.setvariable variable=custNamespace;]$getcustname"
-            #   replacestring='name: $getcustname'
-            # yq -i '.metadata.name |= $getcustname' templates/tenants/deployments/nbsampleapp.yaml
-            # yq -r .test.database templates/tenants/deployments/nbsampleapp.yaml
-            yq '.metadata.name = `echo $getcustname`' templates/tenants/deployments/nbsampleapp.yaml
-  done 
+for im in $(yq eval -o=j templates/tenants/customers.yaml | jq -cr '.deployments[]'); do
+      name=$(echo $im | jq -r '.name' -)
+      size=$(echo $im | jq -r '.size' -)
+      environment=$(echo $im | jq -r '.environment' -)
+      tenantname=$(echo $im | jq -r '.tenantname' -)
+      deploymentgroup=$(echo $im | jq -r '.deploymentgroup' -)
+      export custname=$name 
+      export custsize=$size 
+      export custenvironment=$environment
+      export custtenant=$tenantname
+      export custdeployment=$deploymentgroup
+      echo $custname
+      yq '.metadata.name = $custname' templates/tenants/deployments/nbsampleapp.yaml
+done
