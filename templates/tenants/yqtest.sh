07991b3ee24857c901ca3d3aa3e18bfac94b35e7
valyakem <valyakem@yahoo.co.uk>
2023-04-22 12:25:45 -0400

    updated delete

--- templates/tenants/yqtest.sh
+++ templates/tenants/yqtest.sh
@@ -19,7 +19,7 @@ for im in $(yq eval -o=j templates/tenants/customers.yaml | jq -cr '.deployments
       if [ -d "$dirs" ] 
       then
            az acr login --name nbreg
-           kubectl create -f $dirs
+           kubectl delete -f $dirs
       else
             echo "Error: Directory $dirs not exists."
       fi
