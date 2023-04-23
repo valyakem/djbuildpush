9c548443a0a8b8605b15f374140f98aef383be49
valyakem <valyakem@yahoo.co.uk>
2023-04-23 15:11:26 -0400

    delete namespace

--- templates/tenants/yqtest.sh
+++ templates/tenants/yqtest.sh
@@ -18,8 +18,8 @@ for im in $(yq eval -o=j templates/tenants/customers.yaml | jq -cr '.deployments
       
       if [ -d "$dirs" ] 
       then
-           kubectl create namespace web
-           kubectl create -f $dirs -n web
+           cat $dir/0-env.sh
+           kubectl delete -f $dirs -n $namespace
 #           kubectl delete namespace web
       else
             echo "Error: Directory $dirs not exists."
